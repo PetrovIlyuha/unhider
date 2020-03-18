da61933ad977d827a7c16c463927a185d161859f
Ilya Petrov <dnamix1@gmail.com>
2020-03-18 15:00:12 +0300

    pin creation mutation handled by GraphQLClient implemented

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,6 +1,9 @@
 import React, { useState, useContext } from 'react';
+import { GraphQLClient } from 'graphql-request';
 import axios from 'axios';
 import Context from '../../state_manager/context';
+import { CREATE_PIN_MUTATION } from '../../graphql/mutations';
+
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
@@ -11,15 +14,38 @@ import ClearIcon from '@material-ui/icons/Clear';
 import SaveIcon from '@material-ui/icons/SaveTwoTone';
 
 const CreatePin = ({ classes }) => {
-  const { dispatch } = useContext(Context);
+  const { state, dispatch } = useContext(Context);
   const [title, setTitle] = useState('');
   const [image, setImage] = useState('');
   const [content, setContent] = useState('');
+  const [submitting, setSubmitting] = useState(false);
 
   const handleSubmit = async e => {
-    e.preventDefault();
-    const url = await handleImageUpload();
-    console.log({ title, image, url, content });
+    setSubmitting(true);
+    try {
+      e.preventDefault();
+      const idToken = window.gapi.auth2
+        .getAuthInstance()
+        .currentUser.get()
+        .getAuthResponse().id_token;
+      const client = new GraphQLClient('http://localhost:4000/graphql', {
+        headers: { authorization: idToken }
+      });
+      const url = await handleImageUpload();
+      const {
+        draft: { latitude, longitude }
+      } = state;
+      const variables = { title, image: url, content, latitude, longitude };
+      const { createPin } = await client.request(
+        CREATE_PIN_MUTATION,
+        variables
+      );
+      console.log('Pin created', { createPin });
+      handleDeleteDraft();
+    } catch (err) {
+      setSubmitting(false);
+      console.error('Error creating pin: ', err);
+    }
   };
 
   const handleDeleteDraft = () => {
@@ -105,7 +131,7 @@ const CreatePin = ({ classes }) => {
           className={classes.button}
           variant="contained"
           color="secondary"
-          disabled={!title.trim() || !content.trim() || !image}
+          disabled={!title.trim() || !content.trim() || !image || submitting}
           onClick={handleSubmit}
         >
           Submit
